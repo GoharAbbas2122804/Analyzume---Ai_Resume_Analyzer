2bc9efb3bf77425def0aa6c218c49f4e9918f0e7
GoharAbbas <GoharAbbas2122804@gmail.com>
2025-07-30 19:57:19 +0500

    finished the auth , cloud storage , resume analyzer ai features

---
+++ types/pdfjs.d.ts
@@ -0,0 +1,14 @@
+declare module 'pdfjs-dist/build/pdf.mjs' {
+  const pdfjs: any;
+  export = pdfjs;
+}
+
+declare module 'pdfjs-dist/legacy/build/pdf.js' {
+  const pdfjs: any;
+  export = pdfjs;
+}
+
+declare module 'pdfjs-dist' {
+  const pdfjs: any;
+  export = pdfjs;
+} 
